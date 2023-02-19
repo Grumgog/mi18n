98494a5d1e20b3964e77bb34ffb9341d6318bb2a
Vasiliy Nikonov <grumgog@gmail.com>
2023-02-19 21:11:29 +1100

    clear project files

---
+++ src/general/dirs.rs
@@ -0,0 +1,16 @@
+use std::{env, path::PathBuf};
+
+use clap::ArgMatches;
+
+pub fn get_directory(args: &ArgMatches) -> PathBuf {
+    if let Some(p) = args.get_one::<PathBuf>("dir") {
+        if p.is_relative() {
+            env::current_dir().unwrap().join(p)
+        } else {
+            p.to_path_buf()
+        }
+    } else {
+        // if dir is not in args -> default current!
+        env::current_dir().expect("unable get current dir!")
+    }
+}
